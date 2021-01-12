65d42cb60421e34111fc3e153935dab83bd98c4d
Jekyllz <jekyllsmagic@gmail.com>
2021-01-12 17:36:23 +0000

    Create pts_spammer.sh

---
+++ pts_spammer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo "these pts are available"
+/bin/ls /dev/pts
+echo "which pts do you want to annoy? : "
+read ans
+
+
+for i in {0..20};
+
+do sleep 1;
+
+echo "hello $i times" >> /dev/pts/$ans;
+
+done
