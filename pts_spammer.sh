f684841326f4fbb17f3e54a1d43f93e1de8c52cb
Jekyllz <jekyllsmagic@gmail.com>
2021-01-13 11:53:46 +0000

    Update pts_spammer.sh

--- pts_spammer.sh
+++ pts_spammer.sh
@@ -1,16 +1,31 @@
+
 #!/bin/bash
 
-
-echo "these pts are available"
-ls /dev/pts
+echo "these pts/users are available"
+w
 echo "which pts do you want to annoy? : "
-read ans
-
-
-for i in {0..20};
-
-do sleep 1;
-
-echo "hello $i times" >> /dev/pts/$ans;
-
+read n
+echo "options: "
+echo "warning     1"
+echo "spam random 2"
+read nn
+if [ $nn -eq 1 ];
+then
+echo "spamming warning..."
+echo " "  >> /dev/pts/$n
+echo "#############################################" >> /dev/pts/$n
+echo "YOU ARE AN UNAUTHRORIZED USER" >> /dev/pts/$n
+echo " I WILL COUNT TO 10 THEN KILL YOUR CONNECTION" >> /dev/pts/$n
+echo "#############################################" >> /dev/pts/$n
+for i in {0..10}
+do echo "$i Mississippi..." >> /dev/pts/$n
+sleep 1
 done
+pkill -9 -t pts/$n
+elif [ $nn -eq 2 ];
+then
+echo "okay - spamming random .."
+cat /dev/urandom >> /dev/pts/$n
+else
+echo "Sorry, try for the next time"
+fi
